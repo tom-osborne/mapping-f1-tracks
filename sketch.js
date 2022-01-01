f9aa4f53f8ddeb219dd74beb9010b71637db754d
IamStanStill <tom.j.osborne@gmail.com>
2022-01-01 18:03:37 +0000

    basics working

--- sketch.js
+++ sketch.js
@@ -2,19 +2,35 @@ let canvas;
 let myMap;
 const key = 'pk.eyJ1IjoidG9tLW9zYm9ybmUiLCJhIjoiY2t4dnluMm96MjFreDJ1bXZpOWRqb2FvbSJ9.sHBUciwxYXsIutZ5BxM5ZA';
 const mappa = new Mappa('MapboxGL', key);
+let track_data;
 
 const options = {
     lat: 0,
     lng: 0,
     zoom: 1,
     style: 'mapbox://styles/mapbox/traffic-night-v2',
-  };
+};
+
+function preload() {
+    track_data = loadJSON('data/f1-tracks.json');
+}
 
 function setup() {
     canvas = createCanvas(windowWidth, windowHeight);
 
     myMap = mappa.tileMap(options);        
     myMap.overlay(canvas);
-  }
+
+    strokeWeight(8);
+    stroke(255);
+    for(let track in track_data){
+        let lat = map(track_data[track].lat, -90, 90, 0, windowWidth);
+        let lon = map(track_data[track].lon, -180, 180, 0, windowHeight);
+
+        point(lat, lon);
+    }
+}
   
-  function draw() {}
+function draw() {}
+
+
