8b1d9647502ec2d7b001f5e00f5876375cd0b195
IamStanStill <tom.j.osborne@gmail.com>
2022-01-01 16:06:39 +0000

    map setup

--- sketch.js
+++ sketch.js
@@ -1,8 +1,20 @@
+let canvas;
+let myMap;
+const key = 'pk.eyJ1IjoidG9tLW9zYm9ybmUiLCJhIjoiY2t4dnluMm96MjFreDJ1bXZpOWRqb2FvbSJ9.sHBUciwxYXsIutZ5BxM5ZA';
+const mappa = new Mappa('MapboxGL', key);
+
+const options = {
+    lat: 0,
+    lng: 0,
+    zoom: 1,
+    style: 'mapbox://styles/mapbox/traffic-night-v2',
+  };
+
 function setup() {
-    createCanvas(400, 400);
+    canvas = createCanvas(windowWidth, windowHeight);
+
+    myMap = mappa.tileMap(options);        
+    myMap.overlay(canvas);
   }
   
-  function draw() {
-    background(0);
-    ellipse(100, 100, 8)
-}
+  function draw() {}
